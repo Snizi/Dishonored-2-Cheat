46a9b490db42037de37fede30dcf57f7bcf4507c
Luan Simões <41025154+Snizi@users.noreply.github.com>
2020-10-02 21:29:59 -0300

    Update dllmain.cpp

--- Dishonored2/dllmain.cpp
+++ Dishonored2/dllmain.cpp
@@ -101,7 +101,7 @@ BOOL APIENTRY DllMain( HMODULE hModule,
                        LPVOID lpReserved
                      )
 {
-if(ul_reason_for_call == DLL_PROCESS_ATTACH
+if(ul_reason_for_call == DLL_PROCESS_ATTACH)
     {
        CreateThread(nullptr, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(HackThread), hModule, 0, nullptr);
     }
